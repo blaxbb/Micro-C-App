efc732197e3c4136fc3833cf001583107b7fa6ca
Brian Barrett <blaxbb@gmail.com>
2020-08-07 03:12:59 -0400

    android release a1

--- micro-c-app/micro-c-app.Android/MainActivity.cs
+++ micro-c-app/micro-c-app.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace micro_c_app.Droid
 {
-    [Activity(Label = "micro_c_app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Micro C App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
