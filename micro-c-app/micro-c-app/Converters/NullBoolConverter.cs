a8f732e1785e88a1eb4f874c238da277ea910ad5
Brian Barrett <blaxbb@gmail.com>
2020-08-12 19:01:11 -0400

    collapse empty text fields for build page

---
+++ micro-c-app/micro-c-app/Converters/NullBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace micro_c_app
+{
+    public class NullBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
