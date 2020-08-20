773352289427ad0c98fcdf278fc627f0c62b3bc9
Brian Barrett <blaxbb@gmail.com>
2020-08-19 23:36:03 -0400

    prompt for settings on first open

--- micro-c-app/micro-c-app/AppShell.xaml.cs
+++ micro-c-app/micro-c-app/AppShell.xaml.cs
@@ -13,7 +13,19 @@ namespace micro_c_app
             Routing.RegisterRoute(nameof(SearchPage), typeof(SearchPage));
             Routing.RegisterRoute(nameof(BuildPage), typeof(BuildPage));
             Routing.RegisterRoute(nameof(QuotePage), typeof(QuotePage));
-            var i = Items;
+
+            if(SettingsPage.VersionPrompt() < SettingsPage.CURRENT_VERSION_PROMPT)
+            {
+                Device.InvokeOnMainThreadAsync(async () =>
+                {
+                    var goToSettings = await DisplayAlert("Check settings", "You should set your store, sales ID, and tax rate before using the app.", "Ok", "Cancel");
+                    SettingsPage.VersionPrompt(SettingsPage.CURRENT_VERSION_PROMPT);
+                    if (goToSettings)
+                    {
+                        await Navigation.PushAsync(new SettingsPage());
+                    }
+                });
+            }
         }
 
         private void DisplaySettings(object sender, EventArgs e)
--- micro-c-app/micro-c-app/Views/SettingsPage.xaml.cs
+++ micro-c-app/micro-c-app/Views/SettingsPage.xaml.cs
@@ -12,6 +12,9 @@ namespace micro_c_app.Views
         public const string PREF_TAX_RATE = "tax_rate";
         public const string PREF_CSV_QUOTE = "csv_with_quote";
         public const string PREF_THEME = "theme";
+        public const string PREF_VERSION_PROMPT = "version_prompt";
+
+        public const int CURRENT_VERSION_PROMPT = 1;
         public SettingsPage()
         {
             InitializeComponent();
@@ -23,6 +26,9 @@ namespace micro_c_app.Views
         public static float TaxRateFactor() => (TaxRate() * .01f) + 1;
         public static bool IncludeCSVWithQuote() => Preferences.Get(PREF_CSV_QUOTE, true);
         public static OSAppTheme Theme() => (OSAppTheme)Preferences.Get(PREF_THEME, 0);
+        public static int VersionPrompt() => Preferences.Get(PREF_VERSION_PROMPT, 0);
+
+
 
         public static void StoreID(string id) => Preferences.Set(PREF_SELECTED_STORE, id);
         public static void SalesID(string id) => Preferences.Set(PREF_SALES_ID, id);
@@ -30,5 +36,6 @@ namespace micro_c_app.Views
         public static void IncludeCSVWithQuote(bool include) => Preferences.Set(PREF_CSV_QUOTE, include);
 
         public static void Theme(OSAppTheme theme) => Preferences.Set(PREF_THEME, (int)theme);
+        public static void VersionPrompt(int value) => Preferences.Set(PREF_VERSION_PROMPT, value);
     }
 }
