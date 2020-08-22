ac6747a80179203d36c53913f71ef6c9a94407f3
Brian Barrett <blaxbb@gmail.com>
2020-08-22 18:35:24 -0400

    forgot missing files.......

---
+++ micro-c-app/micro-c-app/ViewModels/BuildPageViewModel.cs
@@ -0,0 +1,163 @@
+﻿using micro_c_app.Models;
+using micro_c_app.Models.Reference;
+using micro_c_app.Views;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace micro_c_app.ViewModels
+{
+    public class BuildPageViewModel : BaseViewModel
+    {
+        string configID;
+        public string ConfigID { get => configID; set { SetProperty(ref configID, value); } }
+        public ICommand ComponentSelectClicked { get; }
+
+        public ObservableCollection<BuildComponent> Components { get; }
+        public INavigation Navigation { get; internal set; }
+
+        public float Subtotal => Components.Sum(c => c.Item?.Price ?? 0f);
+        public string TaxedTotal => $"({SettingsPage.TaxRate()})% ${(Subtotal * SettingsPage.TaxRateFactor()).ToString("#0.00")}";
+
+        public static float CurrentSubTotal { get; set; }
+
+        public ICommand SendQuote { get; }
+        public ICommand Reset { get; }
+
+        public BuildPageViewModel()
+        {
+            Title = "Build";
+
+            MessagingCenter.Subscribe<BuildComponentViewModel>(this, "selected", BuildComponentSelected);
+            MessagingCenter.Subscribe<BuildComponentViewModel>(this, "new",      BuildComponentNew);
+            MessagingCenter.Subscribe<BuildComponentViewModel>(this, "removed",  BuildComponentRemove);
+            MessagingCenter.Subscribe<BuildComponentViewModel, PlanTier>(this, "add_plan", BuildComponentAddPlan);
+
+            if (RestoreState.Instance.BuildComponents == null)
+            {
+                Components = new ObservableCollection<BuildComponent>();
+                SetupDefaultComponents();
+            }
+            else
+            {
+                Components = new ObservableCollection<BuildComponent>(RestoreState.Instance.BuildComponents);
+                foreach(var comp in Components)
+                {
+                    comp.AddDependencies(FieldContainsDependency.Dependencies);
+                }
+            }
+
+            ConfigID = "ZZZ";
+            ComponentSelectClicked = new Command<BuildComponent>(async (BuildComponent comp) =>
+            {
+                var componentPage = new BuildComponentPage();
+                if (componentPage.BindingContext is BuildComponentViewModel vm)
+                {
+                    vm.Component = comp;
+                }
+                componentPage.Setup();
+                await Navigation.PushAsync(componentPage);
+            });
+
+            SendQuote = new Command(async () => await QuotePageViewModel.DoSendQuote(Components.Where(c => c.Item != null).Select(c => c.Item)));
+
+            Reset = new Command(async () =>
+            {
+                await Device.InvokeOnMainThreadAsync(async () => {
+                    var reset = await Shell.Current.DisplayAlert("Reset", "Are you sure you want to reset the quote?", "Yes", "No");
+                    if (reset)
+                    {
+                        Components.Clear();
+                        SetupDefaultComponents();
+                        Save();
+                    }
+                });
+            });
+
+            Components.CollectionChanged += (sender, args) => { Save(); };
+        }
+
+        void SetupDefaultComponents()
+        {
+            foreach (BuildComponent.ComponentType t in Enum.GetValues(typeof(BuildComponent.ComponentType)))
+            {
+                if (BuildComponent.MaxNumberPerType(t) > 0)
+                {
+                    var comp = new BuildComponent() { Type = t };
+                    comp.AddDependencies(FieldContainsDependency.Dependencies);
+                    Components.Add(comp);
+                }
+            }
+        }
+
+        private void Save()
+        {
+            RestoreState.Instance.BuildComponents = Components.ToList();
+            RestoreState.Save();
+        }
+
+        private void BuildComponentRemove(BuildComponentViewModel updated)
+        {
+            var emptyComponents = Components.Where(c => updated.Component.Type == c.Type && c.Item == null).ToList();
+
+            var count = emptyComponents.Count;
+            ///
+            // for types that we don't want the user to add manually, remove all empty items, othewise leave one for interaction
+            //
+            var stop = BuildComponent.MaxNumberPerType(updated.Component.Type) == 0 ? count : count - 1;
+            for(int i = 0; i < stop; i++)
+            {
+                Components.Remove(emptyComponents[i]);
+            }
+            BuildComponentSelected(updated);
+        }
+
+        private void BuildComponentNew(BuildComponentViewModel updated)
+        {
+            var existing = Components.Count(d => d.Type == updated.Component.Type);
+            if(existing < BuildComponent.MaxNumberPerType(updated.Component.Type))
+            {
+                var newItem = new BuildComponent()
+                {
+                    Type = updated.Component.Type,
+                };
+                foreach (var d in updated.Component.Dependencies)
+                {
+                    var clone = d.Clone();
+                    newItem.AddDependency(clone);
+                    d.Other(updated.Component).AddDependency(clone);
+                }
+                var index = Components.IndexOf(updated.Component);
+                Components.Insert(index + 1, newItem);
+            }
+
+            BuildComponentSelected(updated);
+        }
+
+
+        private void BuildComponentSelected(BuildComponentViewModel updated)
+        {
+            foreach (var depend in updated?.Component.Dependencies)
+            {
+                depend.Other(updated.Component)?.OnDependencyStatusChanged();
+            }
+
+            CurrentSubTotal = Subtotal;
+            updated.Component.OnDependencyStatusChanged();
+            OnPropertyChanged(nameof(Components));
+            OnPropertyChanged(nameof(Subtotal));
+            OnPropertyChanged(nameof(TaxedTotal));
+            Navigation.PopAsync();
+            Save();
+        }
+
+        public void BuildComponentAddPlan(BuildComponentViewModel vm, PlanTier tier)
+        {
+            Components.Add(new BuildComponent() { Type = BuildComponent.ComponentType.Plan, Item = new Item() { Name = $"{tier.Duration} year protection on {vm?.Component?.Item?.Name}", Price = tier.Price } });
+            BuildComponentSelected(vm);
+        }
+    }
+}
