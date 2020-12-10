8985955c86c9df571ce1f7ab939d2c8dc2ace57d
Brian Barrett <blaxbb@gmail.com>
2020-12-09 23:35:47 -0500

    forgot to commit negateconverter.cs

---
+++ micro-c-app/micro-c-app/Converters/NegateConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace micro_c_app
+{
+    public class NegateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool b)
+            {
+                return !b;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+            {
+                return !b;
+            }
+            return false;
+        }
+    }
+}
