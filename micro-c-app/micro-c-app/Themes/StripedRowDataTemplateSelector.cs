a253e10b7b129e0d282cd08728fcfa6d7dbe8074
Brian Barrett <blaxbb@gmail.com>
2020-12-15 01:06:17 -0500

    Make quote page rows stripey

---
+++ micro-c-app/micro-c-app/Themes/StripedRowDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+
+namespace micro_c_app.Themes
+{
+    public class StripedRowDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate EvenRow { get; set; }
+        public DataTemplate OddRow { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if(container is ListView listView)
+            {
+                var items = listView.ItemsSource.Cast<object>().ToList();
+                var index = items.IndexOf(item);
+                return index % 2 == 0 ? EvenRow : OddRow;
+            }
+
+            return EvenRow;
+        }
+    }
+}
