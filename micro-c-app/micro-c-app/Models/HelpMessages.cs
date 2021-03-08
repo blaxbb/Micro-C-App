fa3eab240791c70ebb47283d617caef43edaddab
Brian Barrett <blaxbb@gmail.com>
2021-03-08 05:30:42 -0500

    forgot to commit file

---
+++ micro-c-app/micro-c-app/Models/HelpMessages.cs
@@ -0,0 +1,25 @@
+﻿using micro_c_app.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace micro_c_app.Models
+{
+    public static class HelpMessages
+    {
+        public static string[] Messages = {
+            "You can change categories on this screen in the settings menu.",
+        };
+
+        public static string? GetNextMessage()
+        {
+            var index = SettingsPage.HelpMessageIndex();
+            if(index >= 0 && index < Messages.Length)
+            {
+                return Messages[index];
+            }
+
+            return null;
+        }
+    }
+}
