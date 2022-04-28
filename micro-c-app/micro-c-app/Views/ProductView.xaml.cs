404a297a69d6d6e9578f72c32518e822b78011f2
Brian Barrett <blaxbb@gmail.com>
2022-04-28 03:53:11 -0400

    implement landscape for product details view

--- micro-c-app/micro-c-app/Views/ProductView.xaml.cs
+++ micro-c-app/micro-c-app/Views/ProductView.xaml.cs
@@ -44,6 +44,75 @@ namespace micro_c_app.Views
             StripeStacks();
         }
 
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if(width > height)
+            {
+                priceStack.Orientation = StackOrientation.Vertical;
+                priceStack.Children[0].HorizontalOptions = LayoutOptions.Start;
+                priceStack.Children[1].HorizontalOptions = LayoutOptions.Start;
+                priceStack.Children[2].HorizontalOptions = LayoutOptions.Start;
+
+                Grid.SetColumnSpan(priceStack, 1);
+
+                picture.IsVisible = false;
+
+                detailGrid.Children.Add(tabButtons);
+                Grid.SetRow(tabButtons, 1);
+                Grid.SetColumn(tabButtons, 1);
+                Grid.SetRowSpan(tabButtons, 2);
+
+                detailGrid.RowDefinitions.Last().Height = GridLength.Star;
+                mainGrid.RowDefinitions[0].Height = GridLength.Star;
+                mainGrid.RowDefinitions[1].Height = 0;
+                mainGrid.RowDefinitions[2].Height = 0;
+
+                var tabs = new View[] { inventoryTab, clearanceTab, planTab, specsTab };
+                foreach (var tab in tabs)
+                {
+                    detailGrid.Children.Add(tab);
+                    Grid.SetRow(tab, 3);
+                    Grid.SetColumn(tab, 1);
+                    Grid.SetRowSpan(tab, 3);
+                }
+            }
+            else
+            {
+                priceStack.Orientation = StackOrientation.Horizontal;
+                priceStack.Children[0].HorizontalOptions = LayoutOptions.StartAndExpand;
+                priceStack.Children[1].HorizontalOptions = LayoutOptions.CenterAndExpand;
+                priceStack.Children[2].HorizontalOptions = LayoutOptions.EndAndExpand;
+
+                Grid.SetColumnSpan(priceStack, 2);
+
+                picture.IsVisible = true;
+
+                mainGrid.Children.Add(tabButtons);
+                Grid.SetRow(tabButtons, 1);
+                Grid.SetColumn(tabButtons, 0);
+                Grid.SetRowSpan(tabButtons, 1);
+
+                detailGrid.RowDefinitions.Last().Height = GridLength.Auto;
+                mainGrid.RowDefinitions[0].Height = GridLength.Auto;
+                mainGrid.RowDefinitions[1].Height = GridLength.Auto;
+                mainGrid.RowDefinitions[2].Height = GridLength.Star;
+
+                var tabs = new View[] { inventoryTab, clearanceTab, planTab, specsTab };
+                foreach(var tab in tabs)
+                {
+                    mainGrid.Children.Add(tab);
+                    Grid.SetRow(tab, 2);
+                    Grid.SetColumn(tab, 0);
+                    Grid.SetRowSpan(tab, 1);
+                }
+            }
+            if (width > 0 && height > 0)
+            {
+                InvalidateMeasure();
+            }
+        }
+
         private void StripeStacks()
         {
             var stripeColor = Application.Current.UserAppTheme == OSAppTheme.Dark ||
--- micro-c-app/micro-c-app/Views/SearchPage.xaml.cs
+++ micro-c-app/micro-c-app/Views/SearchPage.xaml.cs
@@ -38,40 +38,111 @@ namespace micro_c_app.Views
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
-            if(width > height)
+            if (width > height)
             {
+                //
+                //page grid contents
+                //
+
                 Grid.SetRow(hintGrid, 0);
-                Grid.SetRow(detailView, 1);
-                Grid.SetRow(emptyView, 1);
-                //Grid.SetRow(searchView, 0);
-
-                //Grid.SetRowSpan(searchView, 2);
-
                 Grid.SetColumn(hintGrid, 0);
-                Grid.SetColumn(detailView, 0);
-                Grid.SetColumn(emptyView, 0);
-                //Grid.SetColumn(searchView, 1);
 
-                //searchView.Orientation = "Vertical";
+                Grid.SetRow(emptyView, 1);
+                Grid.SetColumn(emptyView, 0);
+
+                Grid.SetRow(productView, 1);
+                Grid.SetColumn(productView, 0);
+
+                Grid.SetRow(searchGrid, 0);
+                Grid.SetColumn(searchGrid, 1);
+                Grid.SetRowSpan(searchGrid, 2);
+
+                Grid.SetRow(spacer, 2);
+                Grid.SetColumn(spacer, 0);
+                Grid.SetColumnSpan(spacer, 2);
+
+                searchView.Orientation = "Vertical";
+
+                searchGrid.HeightRequest = -1;
+
                 grid.RowDefinitions[3].Height = 0;
+                grid.ColumnDefinitions[1].Width = GridLength.Star;
+                //
+                // search grid contents
+                //
+
+                searchGrid.ColumnDefinitions[0].Width = GridLength.Star;
+                searchGrid.ColumnDefinitions[1].Width = GridLength.Auto;
+
+                searchGrid.RowDefinitions[0].Height = GridLength.Auto;
+                searchGrid.RowDefinitions[1].Height = GridLength.Star;
+
+                Grid.SetRow(homeButton, 0);
+                Grid.SetColumn(homeButton, 0);
+                homeButton.HorizontalOptions = LayoutOptions.End;
+
+                Grid.SetRow(backButton, 0);
+                Grid.SetColumn(backButton, 1);
+
+                Grid.SetColumn(searchView, 0);
+                Grid.SetRow(searchView, 1);
+                Grid.SetColumnSpan(searchView, 2);
+
             }
             else
             {
+                //
+                //page grid contents
+                //
+
                 Grid.SetRow(hintGrid, 0);
-                Grid.SetRow(detailView, 1);
-                Grid.SetRow(emptyView, 1);
-                //Grid.SetRow(searchView, 2);
-
                 Grid.SetColumn(hintGrid, 0);
-                Grid.SetColumn(detailView, 0);
+
+                Grid.SetRow(emptyView, 1);
                 Grid.SetColumn(emptyView, 0);
-                //Grid.SetColumn(searchView, 0);
 
-                //Grid.SetRowSpan(searchView, 1);
+                Grid.SetRow(productView, 1);
+                Grid.SetColumn(productView, 0);
 
-                //searchView.Orientation = "Horizontal";
-                grid.RowDefinitions[3].Height = new GridLength(1, GridUnitType.Auto);
+                Grid.SetRow(searchGrid, 2);
+                Grid.SetColumn(searchGrid, 0);
+                Grid.SetRowSpan(searchGrid, 1);
+
+                Grid.SetRow(spacer, 3);
+                Grid.SetColumn(spacer, 0);
+                Grid.SetColumnSpan(spacer, 1);
+
+                searchView.Orientation = "Horizontal";
+
+                searchGrid.HeightRequest = 125;
+
+                grid.RowDefinitions[3].Height = GridLength.Auto;
+                grid.ColumnDefinitions[1].Width = 0;
+
+
+                //
+                // search grid contents
+                //
+
+                searchGrid.ColumnDefinitions[0].Width = GridLength.Auto;
+                searchGrid.ColumnDefinitions[1].Width = GridLength.Star;
+
+                searchGrid.RowDefinitions[0].Height = GridLength.Star;
+                searchGrid.RowDefinitions[1].Height = GridLength.Star;
+
+                Grid.SetRow(homeButton, 0);
+                Grid.SetColumn(homeButton, 0);
+                homeButton.HorizontalOptions = LayoutOptions.Center;
+
+                Grid.SetRow(backButton, 1);
+                Grid.SetColumn(backButton, 0);
+
+                Grid.SetColumn(searchView, 1);
+                Grid.SetRow(searchView, 0);
+                Grid.SetRowSpan(searchView, 2);
             }
+
+            InvalidateMeasure();
         }
 
         protected override bool OnBackButtonPressed()
