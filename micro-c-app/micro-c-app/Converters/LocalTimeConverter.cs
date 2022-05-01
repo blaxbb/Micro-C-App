d097bf72eb35f3ba466f6420d2ce932f43ebecc1
Brian Barrett <blaxbb@gmail.com>
2022-05-01 08:40:06 -0400

    Missing file

---
+++ micro-c-app/micro-c-app/Converters/LocalTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace micro_c_app
+{
+    public class LocalTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime time)
+            {
+                return time.ToLocalTime();
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime time)
+            {
+                return time.ToUniversalTime();
+            }
+
+            return value;
+        }
+    }
+}
